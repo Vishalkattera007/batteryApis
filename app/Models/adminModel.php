d6dc13e217a6e16e5a9d3e1f364f360113d76523
AnitaSeth1 <anita.glansa@gmail.com>
2024-10-21 15:11:27 +0530

    Add DealerController and Route

--- app/Models/adminModel.php
+++ app/Models/adminModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class adminmaster extends Model
+class adminModel extends Model
 {
     //
     use HasFactory;
