7fa4d99fc7e9d8e6c84735ae7eb0579ea7c74180
Glansa-Solutions <glansa.solutions777@gmail.com>
2024-10-25 17:07:59 +0530

    Dealer to Customer POST METHOD DONE

--- app/Http/Controllers/Api/subCategoryController.php
+++ app/Http/Controllers/Api/subCategoryController.php
@@ -61,19 +61,6 @@ class subCategoryController extends Controller
      */
     public function create(Request $request)
     {
-        
-
-        // $intoWords = explode(' ', $sub_category_name);
-
-        // if (count($intoWords) == 1) {
-        //     $shortcode = substr($intoWords[0], 0, 3);
-        // } else {
-        //     $shortcode = '';
-        //     foreach ($intoWords as $words) {
-        //         $shortcode .= substr($words, 0, 1);
-        //     }
-        // }
-
         $subcategory_check_duplicate = SubCategoryModel::where('categoryId', $request->categoryId)->where('sub_category_name', $request->sub_category_name)->first();
 
         if($subcategory_check_duplicate){
@@ -94,7 +81,15 @@ class subCategoryController extends Controller
         return response()->json([
             'status' => 200,
             'message' => 'SubCategory created successfully',
-            'data' => $subcategory,
+            'data' => [
+                'categoryId' => $subcategory->categoryId,
+                'sub_category_name' => $subcategory->sub_category_name,
+                'short_code' => $subcategory->short_code, // Access the correct property
+                'created_by' => $subcategory->created_by,
+                'updated_at' => $subcategory->updated_at,
+                'created_at' => $subcategory->created_at,
+                'id' => $subcategory->id,
+            ],
         ], 200);
     }
 
