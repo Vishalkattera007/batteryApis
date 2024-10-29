afda92b2dc7418bf10147ec4bb0475b8dd85cbca
Glansa-Solutions <glansa.solutions777@gmail.com>
2024-10-29 20:04:17 +0530

    error updated

--- app/Http/Controllers/Api/ExcelUploadController.php
+++ app/Http/Controllers/Api/ExcelUploadController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use App\Imports\BatteryMasterImport;
-use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ExcelUploadController extends Controller
 {
@@ -20,21 +20,24 @@ class ExcelUploadController extends Controller
 
             Excel::import(new BatteryMasterImport, $request->file('file'));
 
-            return response()->json(['message' => 'Data imported successfully'], 200);
+            return response()->json([
+                'status' => 200,
+                // 'data' => $request->file('file'),
+                'message' => 'Data imported successfully'], 200);
         } catch (QueryException $e) {
             // Check if the error is a duplicate entry
-            if ($e->getCode() == 23000 ){
+            if ($e->getCode() == 23000) {
                 return response()->json([
-                    'message' => "Duplicate entry found kindly check"
+                    'status' => 409,
+                    'message' => "Duplicate entry found kindly check",
                 ], 409);
             }
 
-
             Log::error('Error importing file: ' . $e->getMessage());
-            return response()->json(['message' => 'An error occurred while importing the file.'], 500);
+            return response()->json(['status' => 500,'message' => 'An error occurred while importing the file.'], 500);
         } catch (\Exception $e) {
             Log::error('Error importing file: ' . $e->getMessage());
-            return response()->json(['message' => $e->getMessage()], 500);
+            return response()->json(['status' => 500,'message' => $e->getMessage()], 500);
         }
     }
 }
